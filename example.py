a8dc9ca3f07f3c6c261e4916da0e46d6199d6039
Alejandro Gomez <alejodiscovery20@gmail.com>
2017-09-25 23:58:06 -0500

    first code to call ebay api

--- example.py
+++ example.py
@@ -1,16 +1,23 @@
-# import requests
+import requests
 import json
 def add_ebay():
-    api_url = 'http://svcs.ebay.com/services/search/FindingService/v1'
-    apikey = '<your api key goes here>'
-    parameters = {
-        'OPERATION-NAME':'findItemsByKeywords',
-        'SERVICE-VERSION':'1.0.0',
-        'SECURITY-APPNAME':apikey,
-        'RESPONSE-DATA-FORMAT':'json',
-        'keywords':'harry potter phoenix'
-    }
+        api_url = 'http://svcs.ebay.com/services/search/FindingService/v1'
+        apikey = 'Alejandr-hello-PRD-c8dfd86bc-77e411ce'
+        parameters = {
+            'OPERATION-NAME':'findItemsByKeywords',
+            'SERVICE-VERSION':'1.0.0',
+            'SECURITY-APPNAME':apikey,
+            'RESPONSE-DATA-FORMAT':'json',
+            'keywords':'harry potter phoenix',
+            'paginationInput.entriesPerPage':3
+        }
+        resp = requests.get(api_url, params=parameters)
+        resp.raise_for_status()
+        data = json.loads(resp.text)
+        
+        items = data['findItemsByKeywordsResponse'][0]['searchResult'][0]['item']
 
-    print json.dumps(parameters, sort_keys=True, indent=4, separators=(',',' : '))
+        print "\n\n"
+        print data['findItemsByKeywordsResponse'][0]['searchResult'][0]['item'][0]['title']
 
 add_ebay()
--- index.js
+++ index.js
@@ -2,20 +2,34 @@ var meli = require('mercadolibre');
 var request = require('request');
 
 let ebay_url = 'http://svcs.ebay.com/services/search/FindingService/v1'
-let apikey = ''
+let apikey = 'Alejandr-hello-PRD-c8dfd86bc-77e411ce'
 
 var parameters = {
     'OPERATION-NAME':'findItemsByKeywords',
     'SERVICE-VERSION':'1.0.0',
     'SECURITY-APPNAME':apikey,
     'RESPONSE-DATA-FORMAT':'json',
-    keywords:'harry potter phoenix'
+    keywords:'harry potter phoenix',
+    'paginationInput.entriesPerPage':3
   };
 
-console.log(datamodel)
+console.log(parameters)
 
 request({url:ebay_url, qs:parameters}, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
+    // console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    
+    var jsonObject = JSON.parse(body);
+    console.log(typeof jsonObject)
+    handleBody(jsonObject);
   });
+
+function handleBody(body){
+  items = body.findItemsByKeywordsResponse[0].searchResult[0].item
+  console.log(items)
+}
+
+// https://developer.ebay.com/Devzone/finding/HowTo/GettingStarted_JS_NV_JSON/GettingStarted_JS_NV_JSON.html
+// https://developer.ebay.com/DevZone/finding/CallRef/index.html
+// https://developer.ebay.com/devzone/guides/ebayfeatures/Basics/Call-SandboxTesting.html#CreatingaTestUser
